1397974f83a813895f2da1c831ba519d65c88c88
jonasbqz <josebanquez100@gmail.com>
2026-06-09 22:29:01 +0200

    debug: add admin token logs to troubleshoot 404

--- app/admin/[token]/page.tsx
+++ app/admin/[token]/page.tsx
@@ -13,8 +13,16 @@ export default async function AdminPage({ params }: PageProps) {
     process.env.ADMIN_TOKEN ||
     "control-secreto-2026";
 
+  console.log("DEBUG ADMIN ROUTE:", { token, adminToken });
+
   if (token !== adminToken) {
-    notFound();
+    return (
+      <div style={{ padding: "100px", color: "white", background: "black" }}>
+        <h1>DEBUG ADMIN TOKEN</h1>
+        <p>token recibido: &quot;{token}&quot;</p>
+        <p>token esperado: &quot;{adminToken}&quot;</p>
+      </div>
+    );
   }
 
   return <AdminClient />;
